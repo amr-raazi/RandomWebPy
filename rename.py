4971a3cbeeda6798c33a6d4ac517a8b7db3e01b1
amr-raazi <50888029+amr-raazi@users.noreply.github.com>
2020-09-22 08:06:46 +0530

    Minor changes

--- rename.py
+++ rename.py
@@ -5,9 +5,7 @@ print(os.listdir())
 filelist = glob.glob(("photos"))
 i = 1
 for oldname in filelist:
-    # ignore directories
     if os.path.isfile(oldname):
-        # keep original path
         basepath = os.path.split(oldname)[0]
         newname = os.path.join(basepath, "{}.jpg".format(str(i)))
         i = i + 1
